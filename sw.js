c63fc331e8ea354ab8daa7b9a5ccb8731c6ed00f
samiralchami19-coder <samiralchami19@gmail.com>
2026-09-14 15:51:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // OraNW Service Worker — offline cache
-var CACHE = 'oranw-v5.2';
+var CACHE = 'oranw-v5.3';
 var ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', function (e) {
